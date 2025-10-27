1d4ca4d50887aa43eab6600e0dcde98f7cf6190d
Taif-Asiri <toti4438@gmail.com>
2025-10-27 05:07:50 +0300

    rename backend folder to TrueTix_backend

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    location = models.CharField(max_length=150)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TrueTex_backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TrueTix_backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
