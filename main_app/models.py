ff128b2d81cfd106fb2b0d1a97d404707a375489
Taif-Asiri <toti4438@gmail.com>
2025-10-27 05:42:53 +0300

    Create the second model Ticket

--- main_app/models.py
+++ main_app/models.py
@@ -9,4 +9,13 @@ class Event(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+#Create 2nd model 
+class Ticket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  #Link it with user
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    qr_code = models.CharField(max_length=200, unique=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.event.name} - {self.user.username}"
