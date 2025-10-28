dcc66dd04749c6af7b6ac3b293c1a75f72772a5a
Taif-Asiri <toti4438@gmail.com>
2025-10-28 05:59:57 +0300

    Link views with urls

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,3 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EventViewSet, TicketViewSet, TransferViewSet
+
+router = DefaultRouter()
+router.register(r'events', EventViewSet)
+router.register(r'tickets', TicketViewSet)
+router.register(r'transfers', TransferViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
