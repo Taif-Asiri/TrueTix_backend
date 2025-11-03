80009b2cecced1b926281bd3f30384f695c95ad5
Taif-Asiri <toti4438@gmail.com>
2025-11-03 11:09:34 +0300

    Save changes before cleaning history

--- main_app/urls.py
+++ main_app/urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('register/', RegisterView.as_view(), name='register'),
     path('verify/', VerifyEmailView.as_view(), name='verify'),
+ 
 
 ]
